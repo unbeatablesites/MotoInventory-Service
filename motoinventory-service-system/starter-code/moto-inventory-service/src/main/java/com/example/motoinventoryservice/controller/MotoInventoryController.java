c9f5cc77d33a60d64174f0188fea29f3b8b25e90
anicho91 <a.k.nichols@live.com>
2019-09-27 10:49:11 -0400

    added validation fixes & integrated controllers

--- motoinventory-service-system/starter-code/moto-inventory-service/src/main/java/com/example/motoinventoryservice/controller/MotoInventoryController.java
+++ motoinventory-service-system/starter-code/moto-inventory-service/src/main/java/com/example/motoinventoryservice/controller/MotoInventoryController.java
@@ -1,6 +1,8 @@
 package com.example.motoinventoryservice.controller;
 
+import com.example.motoinventoryservice.dao.MotoInventoryDao;
 import com.example.motoinventoryservice.model.Motorcycle;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,39 +12,31 @@ import java.util.Random;
 @RestController
 public class MotoInventoryController {
 
+    @Autowired
+    MotoInventoryDao motoDao;
+
     @RequestMapping(value = "/motorcycles", method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
     public Motorcycle createMotorcycle(@RequestBody @Valid Motorcycle motorcycle) {
-        Random rnd = new Random();
 
-        motorcycle.setId(rnd.nextInt(9999));
 
-        return motorcycle;
+        return motoDao.addMotorcycle(motorcycle);
     }
 
     @RequestMapping(value = "/motorcycles/{motoId}", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
     public Motorcycle getMotorcycle(@PathVariable int motoId) {
         if (motoId < 1) {
-           throw new IllegalArgumentException("MotoId must be greater than 0.");
+            throw new IllegalArgumentException("MotoId must be greater than 0.");
         }
 
-        Motorcycle moto = new Motorcycle();
-        moto.setId(motoId);
-        moto.setVin("54321");
-        moto.setMake("Ducati");
-        moto.setModel("Multistrada Enduro");
-        moto.setYear("2018");
-        moto.setColor("Red");
-
-        return moto;
+        return motoDao.getMotorcycle(motoId);
     }
 
     @RequestMapping(value = "/motorcycles/{motoId}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteMotorcycle(@PathVariable("motoId") int motoId) {
-        // do nothing here - in a real application we would delete the entry from
-        // the backing data store.
+        motoDao.deleteMotorcycle(motoId);
     }
 
     @RequestMapping(value = "/motorcycles/{motoId}")
